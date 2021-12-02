a778e3ff7ce6366528e402c49f327925264695ec
汉广 <serfend@foxmail.com>
2021-12-02 10:04:41 +0800

    refactor[file]file manage update

---
+++ Configuration/FileHelper/CiperFileExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configuration.FileHelper
+{
+    public class CiperFile
+    {
+        /// <summary>
+        /// 路径
+        /// </summary>
+        public string? Path { get; set; }
+        /// <summary>
+        /// 密钥
+        /// </summary>
+        public byte[] Key { get; set; } = new byte[] { 0xde, 0xad, 0xbe, 0xef, 0x00, 0x01, 0x02, 0x03, 0xbe, 0xef, 0x00, 0x01, 0x01, 0x02, 0x03, 0xbe };
+        public byte[] IV { get; set; } = new byte[] { 0xde, 0xad, 0xbe, 0xef, 0x00, 0x01, 0x02, 0x03, 0xbe, 0xef, 0x00, 0x01, 0x01, 0x02, 0x03, 0xbe };
+        public string? Content { set; get; }
+
+        public void Load()
+        {
+            if (!File.Exists(Path)) return;
+            try
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = Key;
+                    aesAlg.IV = IV;
+
+                    ICryptoTransform encryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    using var fs = new FileStream(Path, FileMode.Open);
+                    using var cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Read);
+                    using var sr = new StreamReader(cs);
+                    Content = sr.ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public void Save()
+        {
+            if (!File.Exists(Path)) File.CreateText(Path);
+            try
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = Key;
+                    aesAlg.IV = IV;
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                    using var fs = new FileStream(Path, FileMode.Open);
+                    using var cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Write);
+                    using var sw = new StreamWriter(cs);
+                    sw.Write(Content);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
--- PingProtector.BLL/Updater/FileServerUpdater.cs
+++ PingProtector.BLL/Updater/FileServerUpdater.cs
@@ -11,111 +11,113 @@ using System.Threading;
 
 namespace PingProtector.BLL.Updater
 {
-	public class FileServerUpdater
-	{
-		public const string UpdatePath = "/file/load?filepath=/tools/sgt&filename=everynet.exe";
-		private const string DownLoadPath = "/file/staticFile/";
-		private List<string> Host { get; set; }
-		private Reg updateRecord = new Reg().In("setting");
+    public class FileServerUpdater
+    {
+        public const string UpdatePath = "/file/load?filepath=/tools/sgt&filename=everynet.exe";
+        private const string DownLoadPath = "/file/staticFile/";
+        private List<string> Host { get; set; }
+        private Reg updateRecord = new Reg().In("setting");
 
-		public event EventHandler<NewVersionEventArgs> OnNewVersion;
+        public event EventHandler<NewVersionEventArgs> OnNewVersion;
 
-		public Timer checkUpdate;
+        public Timer checkUpdate;
 
-		public FileServerUpdater(List<string> host)
-		{
-			this.Host = host;
-			CheckUpdate(null);
-			checkUpdate = new Timer(new TimerCallback(CheckUpdate), null, 0, 60000);
-		}
+        public FileServerUpdater(List<string> host)
+        {
+            this.Host = host;
+            CheckUpdate();
+            checkUpdate = new Timer(new TimerCallback(o => CheckUpdate()), null, 0, 60000);
+            OnNewVersion += (s, e) => { };
+            OnNewVersion?.Invoke(this, new NewVersionEventArgs());
+        }
 
-		public void CheckUpdate(object v)
-		{
-			var currentUpdateInfo = updateRecord.GetInfo("CurrentUpdate", "2020-1-1");
-			DateTime.TryParse(currentUpdateInfo, out var currentUpdate);
-			if (currentUpdate == null) currentUpdate = DateTime.Now.AddYears(-1);
-			foreach (var host in Host)
-			{
-				var message = new HttpRequestMessage(HttpMethod.Get, new Uri($"http://{host}{UpdatePath}"));
-				if (RunUpdate(message, currentUpdate, host)) break;
-			}
-		}
+        public void CheckUpdate()
+        {
+            var currentUpdateInfo = updateRecord.GetInfo("CurrentUpdate", "2020-1-1");
+            var success = DateTime.TryParse(currentUpdateInfo, out var currentUpdate);
+            if (!success) currentUpdate = DateTime.Now.AddYears(-1);
+            foreach (var host in Host)
+            {
+                var message = new HttpRequestMessage(HttpMethod.Get, new Uri($"http://{host}{UpdatePath}"));
+                if (RunUpdate(message, currentUpdate, host)) break;
+            }
+        }
 
-		private bool RunUpdate(HttpRequestMessage message, DateTime currentUpdate, string host)
-		{
-			using (var http = new HttpClient())
-			{
-				try
-				{
-					var r = http.SendAsync(message).Result;
-					if (r.StatusCode == System.Net.HttpStatusCode.OK)
-					{
-						var content = r.Content.ReadAsStringAsync().Result;
-						var item = JsonConvert.DeserializeObject(content) as Newtonsoft.Json.Linq.JObject;
-						var file = item.SelectToken("data.file");
-						var lastModefy = file.SelectToken("lastModefy")?.ToString();
-						DateTime.TryParse(lastModefy, out var lastModify);
-						if (currentUpdate < lastModify)
-						{
-							var id = file.SelectToken("id")?.ToString();
-							UpdateVersion(host, id, lastModify);
-							return true;
-						}
-					}
-					return false;
-				}
-				catch (Exception)
-				{
-					return false;
-				}
-			}
-		}
+        private bool RunUpdate(HttpRequestMessage message, DateTime currentUpdate, string host)
+        {
+            using (var http = new HttpClient())
+            {
+                try
+                {
+                    var r = http.SendAsync(message).Result;
+                    if (r.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var content = r.Content.ReadAsStringAsync().Result;
+                        var item = JsonConvert.DeserializeObject(content) as Newtonsoft.Json.Linq.JObject;
+                        var file = item?.SelectToken("data.file");
+                        if (file == null) return false;
+                        var lastModefy = file.SelectToken("lastModefy")?.ToString();
+                        DateTime.TryParse(lastModefy, out var lastModify);
+                        if (currentUpdate < lastModify)
+                        {
+                            var id = file.SelectToken("id")?.ToString() ?? "default";
+                            UpdateVersion(host, id, lastModify);
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
 
-		private void UpdateVersion(string host, string id, DateTime lastModify)
-		{
-			var message = new HttpRequestMessage(HttpMethod.Get, new Uri($"http://{host}{DownLoadPath}{id}"));
-			using (var http = new HttpClient())
-			{
-				var file = http.SendAsync(message).Result;
-				var sr = file.Content.ReadAsByteArrayAsync().Result;
-				using (var tmpFile = File.OpenWrite("./update.tmp"))
-				{
-					tmpFile.Write(sr, 0, sr.Length);
-				}
-			}
-			updateRecord.SetInfo("CurrentUpdate", lastModify.ToString());
-			var cmd = new StringBuilder();
-			var exe = Process.GetCurrentProcess().ProcessName;
-			var path = ConfigurationManager.Current.ExecutionPath;
-			var fullPath = ConfigurationManager.Current.ApplicationFullName;
-			cmd.AppendLine($"del {fullPath}");
-			cmd.AppendLine($"copy /y {path}\\update.tmp {path}\\everynet.exe");
-			cmd.AppendLine($"start  {path}\\everynet.exe");
-			cmd.AppendLine($"del {path}\\update.tmp");
-			RunCmdOuter(cmd.ToString());
-		}
+        private void UpdateVersion(string host, string id, DateTime lastModify)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Get, new Uri($"http://{host}{DownLoadPath}{id}"));
+            using (var http = new HttpClient())
+            {
+                var file = http.SendAsync(message).Result;
+                var sr = file.Content.ReadAsByteArrayAsync().Result;
+                using var tmpFile = File.OpenWrite("./update.tmp");
+                tmpFile.Write(sr, 0, sr.Length);
+                OnNewVersion?.Invoke(this, new NewVersionEventArgs() { FileName = id, LastModify = lastModify, Length = sr.Length });
+            }
+            updateRecord.SetInfo("CurrentUpdate", lastModify.ToString());
+            var cmd = new StringBuilder();
+            var exe = Process.GetCurrentProcess().ProcessName;
+            var path = ConfigurationManager.Current.ExecutionPath;
+            var fullPath = ConfigurationManager.Current.ApplicationFullName;
+            cmd.AppendLine($"del {fullPath}");
+            cmd.AppendLine($"copy /y {path}\\update.tmp {path}\\everynet.exe");
+            cmd.AppendLine($"start  {path}\\everynet.exe");
+            cmd.AppendLine($"del {path}\\update.tmp");
+            RunCmdOuter(cmd.ToString());
+        }
 
-		public void RunCmdOuter(string cmd)
-		{
-			var lines = new StringBuilder();
-			lines.AppendLine("timeout /T 3 /NOBREAK");
-			lines.AppendLine(cmd);
-			lines.AppendLine("del update.bat");
-			File.WriteAllText("update.bat", lines.ToString());
-			Process.Start(new ProcessStartInfo()
-			{
-				FileName = "update.bat",
-				UseShellExecute = true,
-				CreateNoWindow = true,
-			});
-			Environment.Exit(0);
-		}
-	}
+        public void RunCmdOuter(string cmd)
+        {
+            var lines = new StringBuilder();
+            lines.AppendLine("timeout /T 3 /NOBREAK");
+            lines.AppendLine(cmd);
+            lines.AppendLine("del update.bat");
+            File.WriteAllText("update.bat", lines.ToString());
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "update.bat",
+                UseShellExecute = true,
+                CreateNoWindow = true,
+            });
+            Environment.Exit(0);
+        }
+    }
 
-	public class NewVersionEventArgs : EventArgs
-	{
-		public DateTime LastModify { get; set; }
-		public int Length { get; set; }
-		public string FileName { get; set; }
-	}
+    public class NewVersionEventArgs : EventArgs
+    {
+        public DateTime LastModify { get; set; }
+        public int Length { get; set; }
+        public string FileName { get; set; }
+    }
 }
