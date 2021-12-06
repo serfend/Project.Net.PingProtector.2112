1290147a7e206e7dbb0e1619b50771500dfb17b6
汉广 <serfend@foxmail.com>
2021-12-06 18:11:20 +0800

    fix[log]use current-domain directory

--- Project.Net.PingProtector.2006/Services/LogServices.cs
+++ Project.Net.PingProtector.2006/Services/LogServices.cs
@@ -13,7 +13,7 @@ namespace Project.Net.PingProtector._2006.Services
         private const string ConfigContent = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiID8+CjxubG9nIHhtbG5zPSJodHRwOi8vd3d3Lm5sb2ctcHJvamVjdC5vcmcvc2NoZW1hcy9OTG9nLnhzZCIKICAgICAgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSI+CgoJPHRhcmdldHM+CgkJPHRhcmdldCB4c2k6dHlwZT0iRmlsZSIgbmFtZT0iZmlsZV9tYWluIiBmaWxlTmFtZT0iJHtiYXNlZGlyfS9sb2dzL2xvZy4ke2V2ZW50LXByb3BlcnRpZXM6ZmlsZW5hbWV9LiR7c2hvcnRkYXRlfS5sb2cgIgogICAgICAgICAgICBsYXlvdXQ9IiR7bG9uZ2RhdGV9ICR7dXBwZXJjYXNlOiR7bGV2ZWx9fSAke21lc3NhZ2V9IiAvPgoJCTx0YXJnZXQgbmFtZT0ibG9nY29uc29sZSIgeHNpOnR5cGU9IkNvbnNvbGUiIC8+Cgk8L3RhcmdldHM+CgoJPHJ1bGVzPgoJCTxsb2dnZXIgbmFtZT0iKiIgbWlubGV2ZWw9IkRlYnVnIiB3cml0ZVRvPSJmaWxlX21haW4iIC8+Cgk8L3J1bGVzPgo8L25sb2c+";
         public static void Init()
         {
-            var currentPath = Application.StartupPath;
+            var currentPath = AppDomain.CurrentDomain.BaseDirectory; // https://zhuanlan.zhihu.com/p/104469966
             var targetPath = $"{currentPath}/logs";
             if (!Directory.Exists(targetPath)) Directory.CreateDirectory(targetPath);
             var config_file = $"{currentPath}/nlog.config";
