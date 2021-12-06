1c5f9032686416c26bea0e2f601dad95e9ee2e3c
汉广 <serfend@foxmail.com>
2021-12-06 18:10:38 +0800

    feat[setup]init

---
+++ Setup/FileMover.cs
@@ -0,0 +1,98 @@
+﻿using Serilog;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinAPI;
+using static WinAPI.FileHandlerExtensions;
+
+namespace Setup
+{
+    public class FileMigrateEventArgs : EventArgs
+    {
+        public string Name { get; set; }
+        public FileStatus FileStatus { get; set; }
+        public FileMigrateEventArgs(string name, FileStatus fileStatus)
+        {
+            Name = name;
+            FileStatus = fileStatus;
+        }
+    }
+    internal class FileMover
+    {
+        public event EventHandler<FileMigrateEventArgs> OnFileMigrate;
+
+        public string SrcPath { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
+        public string DstPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+        public ConcurrentQueue<string> ToMoveFiles { get; set; } = new ConcurrentQueue<string>();
+        /// <summary>
+        /// 初始化文件迁移
+        /// </summary>
+        /// <param name="packageName">包名称</param>
+        /// <param name="srcPath">源目录</param>
+        /// <param name="dstPath">目标目录(默认为ProgramFiles/包名称)</param>
+        public FileMover(string? packageName = null, string? srcPath = null, string? dstPath = null)
+        {
+            if (srcPath != null) SrcPath = srcPath;
+            packageName ??= "UnnamedApp";
+            DstPath = Path.Combine(dstPath ?? DstPath, packageName);
+            Init();
+        }
+        private void Init()
+        {
+            if (!Directory.Exists(DstPath)) Directory.CreateDirectory(DstPath);
+
+        }
+        public void MovePath(string path)
+        {
+            foreach (var p in Directory.GetDirectories(path)) MovePath(p);
+            MoveFiles(path);
+        }
+        public void MoveFiles(string path)
+        {
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+                ToMoveFiles.Enqueue(file);
+        }
+        public void MoveFile(string path)
+        {
+            var newPath = Path.Combine(DstPath, Path.GetFileName(path));
+            Log.Information($"copy file:{newPath}");
+            //new FileInfo(file).Copy(newPath);
+            var f = new FileInfo(path);
+            var newFile = new FileInfo(newPath);
+            var s = newFile.FileCurrentStatus();
+            var args = new FileMigrateEventArgs(newPath, s);
+            OnFileMigrate?.Invoke(this, args);
+            if (s == FileStatus.None || s == FileStatus.NotExist)
+            {
+                f.CopyTo(newPath, true);
+            }
+            else
+            {
+                Log.Error($"file copy fail[{s.ToString()}]:{path}");
+            }
+        }
+        public void CheckProcess()
+        {
+            var process = Process.GetProcesses();
+            var targets = process.Where(i => i.ProcessName == Program.packageName).ToList();
+            foreach (var p in targets)
+            {
+                p.Kill();
+            }
+        }
+        public void Migrate()
+        {
+            CheckProcess();
+            Log.Warning($"start migrating:{SrcPath} -> {DstPath}");
+            MovePath(SrcPath);
+            while (ToMoveFiles.TryDequeue(out var file)) { MoveFile(file); }
+            //new FileInfo(file).Copy(newPath);
+            //new DirectoryInfo(SrcPath).Copy(DstPath);
+        }
+    }
+}
---
+++ Setup/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using Common.PowershellHelper;
+using DotNet4.Utilities.UtilReg;
+using PingProtector.BLL.Shell;
+using Serilog;
+using Setup;
+using System.Windows.Forms;
+
+namespace Setup
+{
+    public static class Program
+    {
+
+        public static string brand = Project.Core.Protector.Main.BrandName;
+        public static string packageName = Project.Core.Protector.Main.PackageName;
+        public static string description = Project.Core.Protector.Main.Description;
+        public static void Main(string[] args)
+        {
+            var targetPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles));
+
+            LogInitializer.Init();
+            Log.Warning($"即将安装{brand},按下回车键继续。");
+            Console.ReadLine();
+
+            Log.Warning($"开始迁移文件");
+            var migrator = new FileMover(packageName, null, targetPath);
+            migrator.OnFileMigrate += (s, e) =>
+            {
+                if (e.FileStatus.HasFlag(WinAPI.FileHandlerExtensions.FileStatus.IsOccupy))
+                {
+
+                }
+            };
+            migrator.Migrate();
+
+            Log.Warning($"开始安装服务");
+            //DSAPI.文件.删除Windows服务(packageName);
+            var exePath = Path.Combine(migrator.DstPath, $"SGTClientPatchServices.exe");
+            DSAPI.文件.将可执行程序注册为Windows服务(exePath, packageName, brand, DSAPI.文件.服务启动方式.自动, "s");
+            //var cmd = $"sc create {packageName} binPath= \"{exePath}\" DisplayName= {brand} start= auto";
+            //new CmdExecutor() { EnableRedirectStandardInput = true, EnableRedirectStandardOutput = true }.CmdRun("注册服务", cmd, null, true);
+            //new CmdExecutor() { EnableRedirectStandardInput = true, EnableRedirectStandardOutput = true }.CmdRun("服务信息", $"sc description {packageName} \"{description}\"", null, true);
+            new Reg($"SYSTEM\\ControlSet001\\Services\\{packageName}", RegDomain.LocalMachine).SetInfo("Description", description);
+            Log.Warning("完成安装");
+            if (MessageBox.Show(null, "安装完成，需要重启", "完成", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                PowerShellHelper.ExecuteCommand($"shutdown -r -t 0");
+        }
+    }
+}
