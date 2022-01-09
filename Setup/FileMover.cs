0dc961639931f6eeed0aaa0ca3b8fd40d1f10021
汉广 <serfend@foxmail.com>
2022-01-10 07:48:29 +0800

    feat[setup]start up method relocate

--- Setup/FileMover.cs
+++ Setup/FileMover.cs
@@ -1,4 +1,4 @@
-﻿using Serilog;
+﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -11,103 +11,104 @@ using static WinAPI.FileHandlerExtensions;
 
 namespace Setup
 {
-    public class FileMigrateEventArgs : EventArgs
-    {
-        public string Name { get; set; }
-        public FileStatus FileStatus { get; set; }
-        public FileMigrateEventArgs(string name, FileStatus fileStatus)
-        {
-            Name = name;
-            FileStatus = fileStatus;
-        }
-    }
-    public static class FileMoverExtensions
-    {
-        /// <summary>
-        /// 将文件从源目录更新为目标目录
-        /// </summary>
-        /// <param name="path"></param>
-        /// <param name="src"></param>
-        /// <param name="dst"></param>
-        /// <returns></returns>
-        public static string SrcToDstPath(this string path, string src, string dst)
-        {
-            return path.Replace(src.TrimEnd('\\'), dst.TrimEnd('\\'));
-        }
-    }
-    internal class FileMover
-    {
-        public event EventHandler<FileMigrateEventArgs> OnFileMigrate;
+	public class FileMigrateEventArgs : EventArgs
+	{
+		public string Name { get; set; }
+		public FileStatus FileStatus { get; set; }
+		public FileMigrateEventArgs(string name, FileStatus fileStatus)
+		{
+			Name = name;
+			FileStatus = fileStatus;
+		}
+	}
+	public static class FileMoverExtensions
+	{
+		/// <summary>
+		/// 将文件从源目录更新为目标目录
+		/// </summary>
+		/// <param name="path"></param>
+		/// <param name="src"></param>
+		/// <param name="dst"></param>
+		/// <returns></returns>
+		public static string SrcToDstPath(this string path, string src, string dst)
+		{
+			return path.Replace(src.TrimEnd('\\'), dst.TrimEnd('\\'));
+		}
+	}
+	public class FileMover
+	{
+		private ILogger<FileMover> logger;
+		public event EventHandler<FileMigrateEventArgs>? OnFileMigrate;
 
-        public string SrcPath { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
-        public string DstPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
-        public ConcurrentQueue<string> ToMoveFiles { get; set; } = new ConcurrentQueue<string>();
-        /// <summary>
-        /// 初始化文件迁移
-        /// </summary>
-        /// <param name="packageName">包名称</param>
-        /// <param name="srcPath">源目录</param>
-        /// <param name="dstPath">目标目录(默认为ProgramFiles/包名称)</param>
-        public FileMover(string? packageName = null, string? srcPath = null, string? dstPath = null)
-        {
-            if (srcPath != null) SrcPath = srcPath;
-            packageName ??= "UnnamedApp";
-            DstPath = Path.Combine(dstPath ?? DstPath, packageName);
-            Init();
-        }
-        private void Init()
-        {
-            if (!Directory.Exists(DstPath)) Directory.CreateDirectory(DstPath);
+		public string SrcPath { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
+		public string DstPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+		public ConcurrentQueue<string> ToMoveFiles { get; set; } = new ConcurrentQueue<string>();
 
-        }
-        public void MovePath(string path)
-        {
-            var newFolderPath = path.SrcToDstPath(SrcPath, DstPath);
-            if (!Directory.Exists(newFolderPath)) Directory.CreateDirectory(newFolderPath);
-            foreach (var p in Directory.GetDirectories(path)) MovePath(p);
-            MoveFiles(path);
-        }
-        public void MoveFiles(string path)
-        {
-            var files = Directory.GetFiles(path);
-            foreach (var file in files)
-                ToMoveFiles.Enqueue(file);
-        }
-        public void MoveFile(string path)
-        {
-            var newPath = path.SrcToDstPath(SrcPath, DstPath);
-            Log.Information($"copy file:{Path.GetFileName(path)} -> {newPath}");
-            //new FileInfo(file).Copy(newPath);
-            var f = new FileInfo(path);
-            var newFile = new FileInfo(newPath);
-            var s = newFile.FileCurrentStatus();
-            var args = new FileMigrateEventArgs(newPath, s);
-            OnFileMigrate?.Invoke(this, args);
-            if (s == FileStatus.None || s == FileStatus.NotExist)
-                f.CopyTo(newPath, true);
-            else
-            {
-                Log.Error($"file copy fail[{s}]:{path}");
-            }
-        }
-        public void CheckProcess()
-        {
-            var process = Process.GetProcesses();
-            var targets = process.Where(i => i.ProcessName == Program.packageName || i.ProcessName == Program.servicesName).ToList();
-            foreach (var p in targets)
-            {
-                Log.Warning($"关闭进程[{p.Id}]{p.ProcessName}");
-                p.Kill();
-            }
-        }
-        public void Migrate()
-        {
-            CheckProcess();
-            Log.Warning($"start migrating:{SrcPath} -> {DstPath}");
-            MovePath(SrcPath);
-            while (ToMoveFiles.TryDequeue(out var file)) { MoveFile(file); }
-            //new FileInfo(file).Copy(newPath);
-            //new DirectoryInfo(SrcPath).Copy(DstPath);
-        }
-    }
+		/// <summary>
+		/// 初始化文件迁移
+		/// </summary>
+		/// <param name="packageName">包名称</param>
+		/// <param name="srcPath">源目录</param>
+		/// <param name="dstPath">目标目录(默认为ProgramFiles/包名称)</param>
+		public FileMover(ILogger<FileMover> logger)
+		{
+			this.logger = logger;
+		}
+		public void Init(string? packageName = null, string? srcPath = null, string? dstPath = null)
+		{
+			if (!Directory.Exists(DstPath)) Directory.CreateDirectory(DstPath);
+			if (srcPath != null) SrcPath = srcPath;
+			packageName ??= "UnnamedApp";
+			DstPath = Path.Combine(dstPath ?? DstPath, packageName);
+		}
+		public void MovePath(string path)
+		{
+			var newFolderPath = path.SrcToDstPath(SrcPath, DstPath);
+			if (!Directory.Exists(newFolderPath)) Directory.CreateDirectory(newFolderPath);
+			foreach (var p in Directory.GetDirectories(path)) MovePath(p);
+			MoveFiles(path);
+		}
+		public void MoveFiles(string path)
+		{
+			var files = Directory.GetFiles(path);
+			foreach (var file in files)
+				ToMoveFiles.Enqueue(file);
+		}
+		public void MoveFile(string path)
+		{
+			var newPath = path.SrcToDstPath(SrcPath, DstPath);
+			logger.LogInformation($"copy file:{Path.GetFileName(path)} -> {newPath}");
+			//new FileInfo(file).Copy(newPath);
+			var f = new FileInfo(path);
+			var newFile = new FileInfo(newPath);
+			var s = newFile.FileCurrentStatus();
+			var args = new FileMigrateEventArgs(newPath, s);
+			OnFileMigrate?.Invoke(this, args);
+			if (s == FileStatus.None || s == FileStatus.NotExist)
+				f.CopyTo(newPath, true);
+			else
+			{
+				logger.LogError($"file copy fail[{s}]:{path}");
+			}
+		}
+		public void CheckProcess()
+		{
+			var process = Process.GetProcesses();
+			var targets = process.Where(i => i.ProcessName == Program.packageName || i.ProcessName == Program.servicesName).ToList();
+			foreach (var p in targets)
+			{
+				logger.LogWarning($"关闭进程[{p.Id}]{p.ProcessName}");
+				p.Kill();
+			}
+		}
+		public void Migrate()
+		{
+			CheckProcess();
+			logger.LogWarning($"start migrating:{SrcPath} -> {DstPath}");
+			MovePath(SrcPath);
+			while (ToMoveFiles.TryDequeue(out var file)) { MoveFile(file); }
+			//new FileInfo(file).Copy(newPath);
+			//new DirectoryInfo(SrcPath).Copy(DstPath);
+		}
+	}
 }
--- Setup/Program.cs
+++ Setup/Program.cs
@@ -1,16 +1,8 @@
 ﻿
-
-using AutoUpdaterDotNET;
-using Common.PowershellHelper;
-using DotNet4.Utilities.UtilReg;
-using PingProtector.BLL.Shell;
-using Serilog;
-using Setup;
-using Setup.Extensions;
-using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Security.Principal;
-using System.Windows.Forms;
-using static Setup.Extensions.RegisterExtensions;
 
 namespace Setup
 {
@@ -21,129 +13,26 @@ namespace Setup
 		public static string packageName = Project.Core.Protector.Main.PackageName;
 		public static string servicesName = nameof(SGTClientPatchServices);
 		public static string description = Project.Core.Protector.Main.Description;
-		private static FileMover? migrator = null;
-		private enum StartUpType
-		{
-			/// <summary>
-			/// 首次安装应先检查是否有先前的服务，有的话则需要加入到runonce待下次重启时重置新服务
-			/// </summary>
-			Default = 0,
-			/// <summary>
-			/// 为第二次重启，可以重置新服务
-			/// </summary>
-			ToRegisterServices = 1
-		}
-		private static StartUpType GetStartUpType(string[] args)
-		{
-			if (args.Length == 0) return StartUpType.Default;
-			switch (args[0])
-			{
-				case "InstallService":
-					return StartUpType.ToRegisterServices;
-				default:
-					return StartUpType.Default;
-			}
-		}
-		private static List<string> GetClaims()
-		{
-			var user_claims = new WindowsPrincipal(WindowsIdentity.GetCurrent());
-			var claims = new List<string>();
-			foreach (var i in Enum.GetValues(typeof(WindowsBuiltInRole)))
-				if (user_claims.IsInRole((WindowsBuiltInRole)i)) claims.Add(i.ToString());
-			return claims;
-		}
-		private static bool waitingForUpdate = false;
-		private static Updater.Client.Updater updater = new();
 		public static void Main(string[] args)
 		{
-			LogInitializer.Init();
-			var hasNew = RegisterConfigration.Configuration.UpdateAvailable;
-			while (hasNew)
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("./conf/config.json", false)
+				.Build();
+			var app = new ServiceCollection()
+				.AddLogging(loggingBuilder =>
 			{
-				Log.Warning($"发现新版本");
-				RegisterConfigration.Configuration.UpdateAvailable = false;
-				AutoUpdater.CheckForUpdateEvent += (e) =>
-				{
-					updater.StartDownload(e);
-					waitingForUpdate = false;
-				};
-				updater.OnUpdateServerNotSet += (s, e) =>
-				{
-					Log.Warning($"未设置服务器地址，取消下载");
-					Application.Exit();
-				};
-				updater.Start();
-				break;
-			}
-			Waiting();
-			var vNew = new Version(updater.CurrentVersion);
-			var vOld = new Version(RegisterConfigration.Configuration.CurrentVersion ?? "1.0.0");
-			if (vNew <= vOld)
-			{
-				Log.Warning($"当前已安装的版本是更加新的版本({vOld})，取消安装({vNew})");
-				Console.ReadLine();
-				return;
-			}
-			Install(args);
-			RegisterConfigration.Configuration.CurrentVersion = vNew.ToString();
+				loggingBuilder.AddConfiguration(configuration.GetSection("Logging"));
+				loggingBuilder.AddConsole();
+				loggingBuilder.AddDebug();
+			})
+				.AddSingleton(configuration)
+				.AddSingleton<SetupExecutor>()
+				.AddSingleton<FileMover>()
+				.BuildServiceProvider();
+
+			app.GetService<SetupExecutor>()?.Run(args);
 		}
-		private static void Waiting()
-		{
-			int timeout = 120;
-			while (waitingForUpdate && timeout-- > 0)
-			{
-				Thread.Sleep(1000);
-			}
-			if (waitingForUpdate)
-			{
-				Log.Error($"等待服务器下载超时，请联系管理员");
-				Thread.Sleep(10000);
-				Application.Exit();
-			}
-		}
-		private static void Install(string[] args)
-		{
-
-			var startUpType = GetStartUpType(args);
-			var targetPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles));
-			Log.Warning($"即将安装{brand}。\n以[{Environment.UserDomainName}/{Environment.UserName}][{string.Join('|', GetClaims())}]权限运行");
-			Thread.Sleep(5000);
-			Log.Warning($"开始迁移文件");
-			migrator = new FileMover(packageName, null, targetPath);
-			migrator.OnFileMigrate += (s, e) =>
-			{
-				if (e.FileStatus.HasFlag(WinAPI.FileHandlerExtensions.FileStatus.IsOccupy))
-				{
-
-				}
-			};
-			migrator.Migrate();
-
-
-			Log.Warning($"开始安装服务");
-			RegisterServices(startUpType);
-			Log.Warning("完成安装");
-
-			// 临时启动
-			var exePath = Path.Combine(migrator.DstPath, $"ClientPatch.exe");
-			Process.Start(exePath);
-			if (MessageBox.Show(null, "安装完成，需要重启", "完成", MessageBoxButtons.OKCancel) == DialogResult.OK)
-				PowerShellHelper.ExecuteCommand($"shutdown -r -t 0");
-		}
-		private static void RegisterServices(StartUpType startUpType)
-		{
-			var exePath = Path.Combine(migrator.DstPath, $"SGTClientPatchServices.exe");
-			var r = new ServiceRegister(packageName);
-			var count = r.Reg.InnerKey.ValueCount;
-			Log.Warning($"检查服务字段:{count}总数");
-			if (count > 0)
-			{
-				Log.Warning($"移除老版服务");
-				r.RemoveService();
-			}
-			Log.Warning($"安装新版服务");
-			r.InstallService(exePath, brand, ServiceStartType.Auto, description);
-
-		}
+		
 	}
 }
