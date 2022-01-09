827976fd2d6871ee6e3f213effc6c127aa49de94
汉广 <serfend@foxmail.com>
2022-01-09 10:09:04 +0800

    feat[tray]initialize

---
+++ ClientProxyTray/AboutWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientProxyTray
+{
+	/// <summary>
+	/// AboutWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class AboutWindow : MetroWindow
+	{
+		public AboutWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
---
+++ ClientProxyTray/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MahApps.Metro.Controls;
+using System.ComponentModel;
+using System.Windows;
+
+namespace ClientProxyTray
+{
+	public partial class MainWindow : MetroWindow
+	{
+		public MainWindow()
+		{
+		}
+
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			SfTray.Dispose();
+			base.OnClosing(e);
+		}
+		private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+		{
+			var toggleSwitch = sender as ToggleSwitch;
+			if (toggleSwitch == null) return;
+			if (toggleSwitch.IsOn == true)
+			{
+				progress.IsActive = true;
+				progress.Visibility = Visibility.Visible;
+				return;
+			}
+			progress.IsActive = false;
+			progress.Visibility = Visibility.Collapsed;
+		}
+
+		private void FlipViewst_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			var flipview = ((FlipView)sender);
+			switch (flipview.SelectedIndex)
+			{
+				case 0:
+					flipview.BannerText = "Cupcakes!";
+					break;
+				case 1:
+					flipview.BannerText = "Xbox!";
+					break;
+				case 2:
+					flipview.BannerText = "Chess!";
+					break;
+			}
+		}
+	}
+}
