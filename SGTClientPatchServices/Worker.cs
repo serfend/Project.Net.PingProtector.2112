9d825453374a05055777418d09f4ebeebac5f730
汉广 <serfend@foxmail.com>
2022-01-20 21:29:49 +0800

    feat[service]daemon process check process interval

--- SGTClientPatchServices/Worker.cs
+++ SGTClientPatchServices/Worker.cs
@@ -9,19 +9,26 @@ namespace SGTClientPatchServices
 	{
 		private readonly ILogger<Worker> _logger;
 		private Reg processConfig = new Reg().In("ServicesWorker");
+
 		public Worker(ILogger<Worker> logger)
 		{
 			_logger = logger;
 		}
+
 		private void NewProcess()
 		{
+			if (DateTime.Now.Subtract(LastNewProcess).TotalSeconds < 15) return; // 至少15秒后再启动
 			var path = Path.Join(AppDomain.CurrentDomain.BaseDirectory, $"{Project.Core.Protector.Main.PackageName}.exe");
 			var (result, status) = new FileInfo(path).CreateProcess();
+			LastNewProcess = DateTime.Now;
 			if (result != WTSapi32.CreateProcessResult.Success)
 			{
 				_logger.LogError($"进程启动失败:{result}:{status}");
 			}
 		}
+
+		private DateTime LastNewProcess { get; set; }
+
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
 			_logger.LogInformation("services execute : {time}", DateTimeOffset.Now);
@@ -32,10 +39,12 @@ namespace SGTClientPatchServices
 				{
 					case 0:
 						break;
+
 					case 2:
 						_logger.LogInformation("start new process : {time}", DateTimeOffset.Now);
 						NewProcess();
 						break;
+
 					default:
 						break;
 				}
@@ -43,11 +52,13 @@ namespace SGTClientPatchServices
 			}
 			_logger.LogInformation("services process stop : {time}", DateTimeOffset.Now);
 		}
+
 		public override Task StartAsync(CancellationToken stoppingToken)
 		{
 			_logger.LogInformation("services start : {time}", DateTimeOffset.Now);
 			return base.StartAsync(stoppingToken);
 		}
+
 		/// <summary>
 		/// 是否应拉起被守护的进程
 		/// </summary>
@@ -61,11 +72,11 @@ namespace SGTClientPatchServices
 				return isDied;
 			}
 		}
+
 		public override Task StopAsync(CancellationToken stoppingToken)
 		{
 			_logger.LogInformation("services stop : {time}", DateTimeOffset.Now);
 			return base.StopAsync(stoppingToken);
 		}
-
 	}
 }
