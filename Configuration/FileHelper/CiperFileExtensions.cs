efbf29d3e708e6671fca1705fb91e92f7ddf1a92
汉广 <serfend@foxmail.com>
2022-01-20 21:24:29 +0800

    feat[configuration]file helper show exceptions

--- Configuration/FileHelper/CiperFileExtensions.cs
+++ Configuration/FileHelper/CiperFileExtensions.cs
@@ -8,64 +8,70 @@ using System.Threading.Tasks;
 
 namespace Configuration.FileHelper
 {
-    public class CiperFile
-    {
-        /// <summary>
-        /// 路径
-        /// </summary>
-        public string? Path { get; set; }
-        /// <summary>
-        /// 密钥
-        /// </summary>
-        public byte[] Key { get; set; } = new byte[] { 0xde, 0xad, 0xbe, 0xef, 0x00, 0x01, 0x02, 0x03, 0xbe, 0xef, 0x00, 0x01, 0x01, 0x02, 0x03, 0xbe };
-        public byte[] IV { get; set; } = new byte[] { 0xde, 0xad, 0xbe, 0xef, 0x00, 0x01, 0x02, 0x03, 0xbe, 0xef, 0x00, 0x01, 0x01, 0x02, 0x03, 0xbe };
-        public string? Content { set; get; }
+	public class CiperFile
+	{
+		public event EventHandler<UnhandledExceptionEventArgs>? ErrorOccured;
 
-        public string? Load()
-        {
-            if (!File.Exists(Path)) return null;
-            try
-            {
-                using (Aes aesAlg = Aes.Create())
-                {
-                    aesAlg.Key = Key;
-                    aesAlg.IV = IV;
+		/// <summary>
+		/// 路径
+		/// </summary>
+		public string? Path { get; set; }
 
-                    ICryptoTransform encryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-                    using var fs = new FileStream(Path, FileMode.Open);
-                    using var cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Read);
-                    using var sr = new StreamReader(cs);
-                    Content = sr.ReadToEnd();
-                    return Content;
-                }
-            }
-            catch (Exception)
-            {
-            }
-            return null;
-        }
-        public void Save()
-        {
-            if (Path == null) return;
+		/// <summary>
+		/// 密钥
+		/// </summary>
+		public byte[] Key { get; set; } = new byte[] { 0xde, 0xad, 0xbe, 0xef, 0x00, 0x01, 0x02, 0x03, 0xbe, 0xef, 0x00, 0x01, 0x01, 0x02, 0x03, 0xbe };
 
-            File.WriteAllText(Path, String.Empty);
-            try
-            {
-                using (Aes aesAlg = Aes.Create())
-                {
-                    aesAlg.Key = Key;
-                    aesAlg.IV = IV;
-                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-                    using var fs = new FileStream(Path, FileMode.Open);
-                    using var cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Write);
-                    using var sw = new StreamWriter(cs);
-                    sw.Write(Content);
-                }
-            }
-            catch (Exception)
-            {
+		public byte[] IV { get; set; } = new byte[] { 0xde, 0xad, 0xbe, 0xef, 0x00, 0x01, 0x02, 0x03, 0xbe, 0xef, 0x00, 0x01, 0x01, 0x02, 0x03, 0xbe };
+		public string? Content { set; get; }
 
-            }
-        }
-    }
+		public string? Load()
+		{
+			if (!File.Exists(Path)) return null;
+			try
+			{
+				using (Aes aesAlg = Aes.Create())
+				{
+					aesAlg.Key = Key;
+					aesAlg.IV = IV;
+
+					ICryptoTransform encryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+					using var fs = new FileStream(Path, FileMode.Open);
+					using var cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Read);
+					using var sr = new StreamReader(cs);
+					Content = sr.ReadToEnd();
+					return Content;
+				}
+			}
+			catch (Exception ex)
+			{
+				ErrorOccured?.Invoke(this, new UnhandledExceptionEventArgs(ex, false));
+			}
+			return null;
+		}
+
+		public void Save()
+		{
+			if (Path == null) return;
+
+			File.WriteAllText(Path, String.Empty);
+			try
+			{
+				using (Aes aesAlg = Aes.Create())
+				{
+					aesAlg.Key = Key;
+					aesAlg.IV = IV;
+					ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+					using var fs = new FileStream(Path, FileMode.Open);
+					using var cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Write);
+					using var sw = new StreamWriter(cs);
+					sw.Write(Content);
+				}
+			}
+			catch (Exception ex)
+			{
+				ErrorOccured?.Invoke(this, new UnhandledExceptionEventArgs(ex, false));
+			}
+		}
+	}
 }
