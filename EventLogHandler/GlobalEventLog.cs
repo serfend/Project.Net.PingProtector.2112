5bc5115690ef59d29b0eae38c6c1d826663dab1d
汉广 <serfend@foxmail.com>
2022-02-24 19:48:43 +0800

    feat[Logger]use event-custom logger

---
+++ EventLogHandler/GlobalEventLog.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace EventLogHandler
+{
+	public static class GlobalEventLog
+	{
+		public const string LogModuleName = "DefaultModule";
+		public const string LogDefaultSourceName = "ClientPatch";
+		public static Dictionary<string, EventLog> LogDict = new();
+
+		public static EventLog GetLog(string name = LogModuleName)
+		{
+			if (!LogDict.ContainsKey(name))
+			{
+				if (!EventLog.SourceExists(LogDefaultSourceName))
+					EventLog.CreateEventSource(LogDefaultSourceName, name);
+				LogDict[name] = new EventLog(name)
+				{
+					Source = LogDefaultSourceName,
+				};
+			}
+			return LogDict[name];
+		}
+
+		public static EventLog DefaultLogger => GetLog();
+	}
+}
---
+++ EventLogHandler/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:验证平台兼容性", Justification = "<挂起>", Scope = "member", Target = "~P:EventLogHandler.GlobalEventLog.DefaultLogger")]
+[assembly: SuppressMessage("Interoperability", "CA1416:验证平台兼容性", Justification = "<挂起>", Scope = "member", Target = "~F:EventLogHandler.GlobalEventLog.LogDict")]
+[assembly: SuppressMessage("Interoperability", "CA1416:验证平台兼容性", Justification = "<挂起>", Scope = "member", Target = "~M:EventLogHandler.GlobalEventLog.GetLog(System.String)~System.Diagnostics.EventLog")]
